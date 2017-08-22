c6531478973864ef7a1654e48bc4767e311dd75e
manu-91 <emanuele.crespan@gmail.com>
2017-08-22 11:38:38 +0200

    modificata config lista

--- lib/Bubbles/ListBub/ListBubbleConfig.jsx
+++ lib/Bubbles/ListBub/ListBubbleConfig.jsx
@@ -1,9 +1,15 @@
 /*
-*  Name :   ListBubbleConfig.jsx
+*  Name :   BubbleConfig.jsx
 *  Location : /imports/UI/Bubbles
-*  Author: Riccardo Saggese
+*  Author: Nicolò Rigato
 *  Creation Data: 2017-06-27
-*  Description: {class ListBubbleConfig}
+*  Description: {Breve descrizione del file}
+*  ----------------------------------------------
+*  History :
+*  Version: {Versione del file}
+*  Update data: {Data ultima modifica}
+*  Description: {descrizione della modifica}
+*  Author: {Autore della modifica}
 */
 
 import React, { Component } from 'react';
@@ -18,11 +24,12 @@ import CheckBoxList from '../../ui/SingleComponents/CheckBoxList/CheckBoxList.js
 export default class ListBubbleConfig extends AbsList {
     constructor(props){
         super(props);
+        this.state={ num: 0, op:[],title:'', hide:true	};
 		this.display_checklist=this.display_checklist.bind(this);
 		this.add_checklist=this.add_checklist.bind(this);
 		this.send=this.send.bind(this);
 		this.rows = [];
-		this.op=[{id: 1, value: 'Hello World', check:false},{id: 2, value: 'Installation', check:false}];
+		this.op=[{id: 1, value: 'Hello World', check:false},{id: 2, value: 'Installation', check:false}];//arriva un array di stringhe (creare l'array)
     }
 
 	display_checklist(){
@@ -31,7 +38,7 @@ export default class ListBubbleConfig extends AbsList {
 
 	add_checklist(opt){
 		let n=this.state.num;
-        console.log(this.state.num);
+        console.log(opt.length);
 		for(let i=0;i<opt.length;i++){
 		    n++;
 			let id="clopt"+n;
@@ -49,17 +56,21 @@ export default class ListBubbleConfig extends AbsList {
 		this.display_checklist();
 	}
 
-	send(){
+	send(){//mettere apposto sto metodo
 		console.log("sent??");
 	}
 
     render() {
+        let lists=[
+            { title: "asdasd", op: [ "s1", "s2" ] },
+            { title: "werwer", op: [ "r1", "r2" ] }
+        ];
         return (
             <div>
                 <h3>Nome lista:</h3>
                 <LineEdit id="title" placeholder="Inserisci una nome per la lista" updateState={this.titleChange}/><br/>
                 <PushButton buttonName="CheckList" classes="checklist_button btn-sm" handleClick={this.display_checklist}/><br/>
-				<CheckList op={this.op} hide={this.state.hide} add={this.add_checklist}/>
+				<CheckList lists={lists} op={this.op} hide={this.state.hide} add={this.add_checklist}/>
                 {this.rows}<br/>
                 <PushButton buttonName="Add" handleClick={this.addOpt}/><br/>
                 <PushButton buttonName="Send" handleClick={this.send}/>
@@ -73,28 +84,55 @@ export class CheckList extends React.Component{
 	constructor(props){
         super(props);
 		this.state={
-			op:this.props.op
+			op:[]//forse non serve
 		}
-
+//this.props.op
 		this.getCheck=this.getCheck.bind(this);
 		this.add=this.add.bind(this);
+        this.createLists=this.createLists.bind(this);
+        this.ops=[];
+        this.lists=[];
+        this.createLists();
 	}
 
+/*
+[
+{ title:asdasd, op: [ s1, s2 ] },
+]
+*/
+
+    createLists(){
+        let s=0;
+        for(let i=0;i<this.props.lists.length;i++){
+            let op=[];
+            for(let j=0;j<this.props.lists[i].op.length;j++){
+                let o={id: s, value: this.props.lists[i].op[j], check:false}
+                op.push(o);
+                this.ops.push(o);
+                s++;
+            }
+            this.lists.push(
+                <div>
+                    <span>{this.props.lists[i].title}</span>
+                    <CheckBoxList options={op} getCheck={this.getCheck}/>
+                </div>
+            );
+        }
+    }
+
 	getCheck(m){
-		let o=this.state.op;
-		for(let i=0;i<o.length;i++){
-			if(o[i].id===m.id){
-				o[i]=m;
+		for(let i=0;i<this.ops.length;i++){
+			if(this.ops[i].id==m.id){
+				this.ops[i]=m;
 			}
 		}
-		this.setState({op:o});
 	}
 
 	add(){
 		let opt=[];
-		for(let i=0;i<this.state.op.length;i++)
-			if(this.state.op[i].check===true){
-				opt.push(this.state.op[i].value);
+		for(let i=0;i<this.ops.length;i++)
+			if(this.ops[i].check==true){
+				opt.push(this.ops[i].value);
 			}
 		this.props.add(opt);
 	}
@@ -103,7 +141,7 @@ export class CheckList extends React.Component{
         return (
             <div className="checklist_container">
                 <VerticalLayout hide={this.props.hide}>
-					<CheckBoxList options={this.state.op} getCheck={this.getCheck}/>
+					{this.lists}
 					<PushButton buttonName="Add" classes="checklist_button btn-sm" handleClick={this.add}/><br/>
 				</VerticalLayout>
             </div>
@@ -111,6 +149,10 @@ export class CheckList extends React.Component{
         );
     }
 }
+
+/*
+<CheckBoxList options={this.state.op} getCheck={this.getCheck}/>
+*/
 
 /*
 how to use:
